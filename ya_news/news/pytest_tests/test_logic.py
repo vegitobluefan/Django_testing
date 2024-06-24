2c126142f5cb2f864afea7c09e098836724aaa15
vegitobluefan <daniararinov995@gmail.com>
2024-06-24 12:04:30 +0300

    second review 24.06.24

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -3,6 +3,7 @@ from random import choice
 
 import pytest
 from pytest_django.asserts import assertFormError, assertRedirects
+
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
 
